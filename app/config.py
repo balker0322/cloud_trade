92efb30e71f1b64edad712d403bb9551a1204122
inspiron5559 <balker0322@gmail.com>
2020-10-11 21:53:30 +0800

    connecting to binance websocket

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+BINANCE_CREDENTIALS = {
+    'KEY': os.environ.get('BINANCE_KEY'),
+    'SECRET': os.environ.get('BINANCE_SECRET'),
+}
--- app/main.py
+++ app/main.py
@@ -5,7 +5,12 @@ from threading import Lock
 from threading import Thread, Event
 import time
 
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from app.config import BINANCE_CREDENTIALS
 
+
+client = Client(BINANCE_CREDENTIALS['KEY'], BINANCE_CREDENTIALS['SECRET'])
 
 async_mode = None
 app = Flask(__name__)
@@ -15,60 +20,66 @@ socket_ = SocketIO(app, async_mode=async_mode)
 # thread_lock = Lock()
 thread = Thread()
 thread_stop_event = Event()
+conn_key = ""
+count = 0
 
-def emit_data():
-    count = 0
-    while True:
-        print('...')
-        count += 1
-        socket_.emit('my_response',
-            {'data': 'emit_data', 'count': count}, namespace='/test')
-        print('...')
-        socket_.sleep(1)
+def emit_data(msg):
+	global count
+	if msg['e'] != 'error':
+		btc_price = float(msg['c'])
+		socket_.emit('my_response',
+			{'data': str(count), 'count': btc_price}, namespace='/test')
+		socket_.sleep(1)
+		count += 1
+		# if count == 60*60*12:
+		# 	count = 0
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socket_.async_mode)
+	return render_template('index.html', async_mode=socket_.async_mode)
 
 
 @socket_.on('connect', namespace='/test')
 def test_connect():
-    # need visibility of the global thread object
-    global thread
-    print('Client connected')
+	bsm = BinanceSocketManager(client)
+	conn_key = bsm.start_symbol_ticker_socket("BTCUSDT", emit_data)
+	bsm.start()
+	# need visibility of the global thread object
+	# global thread
+	# print('Client connected')
 
-    #Start the random number generator thread only if the thread has not been started before.
-    if not thread.isAlive():
-        print("Starting Thread")
-        thread = socket_.start_background_task(emit_data)
+	# #Start the random number generator thread only if the thread has not been started before.
+	# if not thread.isAlive():
+	#     print("Starting Thread")
+	#     thread = socket_.start_background_task(emit_data)
 
 
 @socket_.on('my_event', namespace='/test')
 def test_message(message):
-    # thread = socket_.start_background_task(emit_data)
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+	# thread = socket_.start_background_task(emit_data)
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		 {'data': message['data'], 'count': session['receive_count']})
 
 
 @socket_.on('my_broadcast_event', namespace='/test')
 def test_broadcast_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		 {'data': message['data'], 'count': session['receive_count']},
+		 broadcast=True)
 
 
 @socket_.on('disconnect_request', namespace='/test')
 def disconnect_request():
-    @copy_current_request_context
-    def can_disconnect():
-        disconnect()
+	@copy_current_request_context
+	def can_disconnect():
+		disconnect()
 
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']},
-         callback=can_disconnect)
+	session['receive_count'] = session.get('receive_count', 0) + 1
+	emit('my_response',
+		 {'data': 'Disconnected!', 'count': session['receive_count']},
+		 callback=can_disconnect)
 
 
